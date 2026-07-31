7c877f10d6dee43a166806639208069a2969176e
sidhu7777 <bvineeth76@gmail.com>
2026-07-31 18:48:00 +0530

    updated the login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,7 +8,7 @@ import comlog from "/logo.svg";
 import axios from "axios";
 import { Eye, EyeOff } from "lucide-react";
 
-const APP_VERSION = "2.0.1";
+const APP_VERSION = "2.1.0";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
